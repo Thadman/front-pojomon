6df77086aeacb5df6e1459f2b0d0abe52f5c003c
Robert Thomas <robertthomas@outlook.com.au>
2020-07-21 13:45:30 +1000

    Corrected routes and added sign-up form + route, tested working

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Monster from "./Components/Monster"
 import Login from "./Components/Login"
+import SignUp from "./Components/SignUp"
+import Monster from "./Components/Monster"
 import NoMatch from "./Components/NoMatch"
 
 class App extends React.Component {
   render() {
-    // const monster = this.state?.monster;
     return (
       <div>
         <BrowserRouter>
           <div>
             <Switch>
               <Route exact path="/login" component={Login} />
-              <Route path="/monster" component={Monster} />
+              <Route exact path="/sign-up" component={SignUp} />
+              <Route exact path="/" component={Monster} />
               <Route path component={NoMatch} />
            </Switch>
           </div>
